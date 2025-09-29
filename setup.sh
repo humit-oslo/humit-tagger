246e999887d0b0a3a141443cc847c851f5c8bfd3
Ahmet Yildirim <ahmet.yildirim@hf.uio.no>
2025-09-29 15:20:04 +0200

    - Fix isse with non existing model directories. This issue happened because we paused the transition to the new model. We will also do a refactoring for better repository management.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+mkdir models/sentence_segmentation
+mkdir models/classification
+cd models/classification
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaa
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xab
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xac
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xad
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xae
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaf
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xag
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/config.json
+cd ..
+cd sentence_segmentation
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaa
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xab
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xac
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xad
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xae
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaf 
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xag
+wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/config.json
+cd ..
+cd ..
+
 ls models/*/x* &>/dev/null
 
 if [[ "$?" -eq 0 ]]; then
