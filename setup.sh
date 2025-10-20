a5b35fb8d7b9b2def31b573655668a94392dfa2c
Ahmet Yildirim <ahmet.yildirim@hf.uio.no>
2025-10-20 16:19:12 +0200

    - Fix some issues with setup script

--- setup.sh
+++ setup.sh
@@ -3,24 +3,25 @@
 mkdir models/sentence_segmentation
 mkdir models/classification
 cd models/classification
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaa
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xab
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xac
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xad
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xae
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaf
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xag
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/config.json
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaa
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xab
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xac
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xad
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xae
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xaf
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/xag
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/classification/config.json
 cd ..
 cd sentence_segmentation
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaa
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xab
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xac
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xad
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xae
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaf 
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xag
-wget https://github.com/humit-oslo/humit-tagger/blob/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/config.json
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaa
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xab
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xac
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xad
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xae
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xaf 
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/xag
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/config.json
+wget https://github.com/humit-oslo/humit-tagger/raw/b3b88c6a3ebfe704f85b0f75a9a04c1f1eda0daa/models/sentence_segmentation/vocab.txt
 cd ..
 cd ..
 
@@ -33,4 +34,4 @@ if [[ "$?" -eq 0 ]]; then
     #rm models/*/x*
 fi
 
-pip3 install -r requirements.txt
+#pip3 install -r requirements.txt
